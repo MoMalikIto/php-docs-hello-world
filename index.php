e06f8ecc82e19c5677e8d6e7dec4cb0b0ccf6950
MoMalikIto <122068779+MoMalikIto@users.noreply.github.com>
2025-11-07 15:12:12 +0000

    Fix date output formatting in index.php

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 
 <?php
 date_default_timezone_set('Europe/London');
-echo date("Y/m/d") . " " . date("h:i:sa") . "<br><br>";
-echo "Day is " . date("D") . "<br>";
+echo date("Y/m/d") . " " . date("h:i:sa") . "<br>";
+echo "Day is " . date("D") . "<br><br>";
 echo "Hello World!";
 ?>
 
